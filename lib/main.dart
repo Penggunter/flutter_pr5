e1c01ee16269e866fcf29737a403990c1ac1dfb9
shevi <shev.igor2002@yandex.ru>
2024-09-17 22:48:48 +0300

    added ios page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pr5/ui/page/android_page.dart';
+import 'package:flutter_pr5/ui/page/ios_page.dart';
 import 'package:flutter_pr5/ui/page/macOs_page.dart';
 import 'package:flutter_pr5/ui/page/windows_page.dart';
 import 'package:flutter_pr5/ui/page/web_page.dart';
@@ -11,16 +12,9 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  // platform variables
-
   //if(Platform.isIos) {
     //platformColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
   //}
-  // WEB https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPW5w9vfdpfAsCQ1Fr5bwDAi96yDBTzTMQJA&s
-  // macOs https://icons.veryicon.com/png/o/miscellaneous/apple-series/macos-2.png
-  // windows https://cdn-icons-png.flaticon.com/512/906/906308.png
-  // ios https://img.icons8.com/?size=512&id=20821&format=png
-  // android https://cdn-icons-png.flaticon.com/512/174/174836.png
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MacOsPage(title: 'Flutter Demo Home Page'),
+      home: IosPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/ui/page/ios_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class IosPage extends StatefulWidget {
+  IosPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<IosPage> createState() => _IosPageState();
+}
+
+class _IosPageState extends State<IosPage> {
+  String _textBar = 'IOS Page';
+  Color _themeColor = Color.fromARGB(255, 0, 0, 0);
+  String _urlIcon = 'https://img.icons8.com/?size=512&id=20821&format=png';
+  String _text = 'Hello ios user';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: _themeColor,
+        title: Text(_textBar,style: TextStyle(color: Colors.white),),
+        
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 128,
+              width: 128,
+              child: Image.network(_urlIcon),
+            ),
+            SizedBox(height: 25,),
+
+            Text(
+              _text,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
